9e4d36918c58038e719edcbb9e88a224829178c8
jiangyukun <187_6810_5877@sina.com>
2017-01-24 17:43:43 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 process.env.NODE_ENV = 'inline'
 
-module.exports = {
-    // devtool: 'cheap-module-eval-source-map',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        './boot/index.js'
-    ],
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-        port: 3000
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: 'http://localhost:3000/static/'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"inline"'
-        }),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        loaders: [
-            {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname},
-            {test: /\.less$/, loader: 'style!css!autoprefixer!less'},
-            {test: /\.scss$/, loader: 'style!css!autoprefixer!sass?sourceMap'},
-            {test: /\.(jpg|png)$/, loader: "url?limit=8192"}
-        ]
+module.exports = [
+    {
+        // devtool: 'cheap-module-eval-source-map',
+        entry: [
+            'webpack-dev-server/client?http://localhost:3000',
+            'webpack/hot/only-dev-server',
+            './boot/index.js'
+        ],
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            port: 3000
+        },
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: 'http://localhost:3000/static/'
+        },
+        plugins: [
+            new ExtractTextPlugin("style.css"),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"inline"'
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        module: {
+            loaders: [
+                {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname},
+                {test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css!autoprefixer!less')},
+                {test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css!autoprefixer!sass?sourceMap')},
+                {test: /\.(jpg|png)$/, loader: "url?limit=8192"}
+            ]
+        }
     }
-}
+]
